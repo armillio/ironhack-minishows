2458d623e5a017faa6c92362d1e4a81855a6adc8
Armando <armando@Armando.local>
2015-02-17 16:00:05 +0100

    Initial Commit

---
+++ miniShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  miniShow
+//
+//  Created by Armando on 17/02/15.
+//  Copyright (c) 2015 Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ miniShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miniShow
+//
+//  Created by Armando on 17/02/15.
+//  Copyright (c) 2015 Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
