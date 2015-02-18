32bdf7dcab03d40d60830850bfb9970a610fa824
Armando <armando@Armando.local>
2015-02-18 20:27:31 +0100

    Tiny bit of refactoring

--- miniShow/ShowViewController.m
+++ miniShow/ShowViewController.m
@@ -68,6 +68,11 @@
 {
     [super viewWillAppear:animated];
     
+    [self makeTheTextViewPretty];
+}
+
+-(void) makeTheTextViewPretty
+{
     NSString *generalInfoTitle = @"General Info";
     NSString *generalInfoContent = @"Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.";
     NSString *overviewTitle = @"Overview";
