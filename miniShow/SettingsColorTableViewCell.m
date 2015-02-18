ebc17db948dd5c640bee680aa9414442209597e0
Jessie Serrino <Jessie.Serrino@tufts.edu>
2015-02-18 17:39:45 +0100

    Settings table cell files

---
+++ miniShow/SettingsColorTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SettingsColorTableViewCell.m
+//  miniShow
+//
+//  Created by Jessie Serrino on 2/18/15.
+//  Copyright (c) 2015 Ironhack. All rights reserved.
+//
+
+#import "SettingsColorTableViewCell.h"
+
+@implementation SettingsColorTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ miniShow/SettingsLayoutTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SettingsLayoutTableViewCell.m
+//  miniShow
+//
+//  Created by Jessie Serrino on 2/18/15.
+//  Copyright (c) 2015 Ironhack. All rights reserved.
+//
+
+#import "SettingsLayoutTableViewCell.h"
+
+@implementation SettingsLayoutTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
