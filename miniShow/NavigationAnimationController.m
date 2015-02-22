4d1affe23332c33e7b2f2c59d5c56c5fe83c7da8
Armando <armando@Armando.local>
2015-02-22 22:11:12 +0100

    Normal animation so I can merge with Jessie's work

--- miniShow/NavigationAnimationController.m
+++ miniShow/NavigationAnimationController.m
@@ -11,11 +11,11 @@
 #import "MainViewController.h"
 #import "ShowViewController.h"
 
-static CGFloat const totalDuration = 0.3;
 
-@interface NavigationAnimationController () <UINavigationControllerDelegate>
-
+@interface  NavigationAnimationController() <UINavigationControllerDelegate>
 @end
+
+static CGFloat const totalDuration = 1.0;
 
 @implementation NavigationAnimationController
 
@@ -39,57 +39,38 @@ static CGFloat const totalDuration = 0.3;
 {
     MainViewController *fromViewController = (MainViewController *)[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     ShowViewController *toViewController = (ShowViewController *)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    
+
     MainViewTableViewCell *cell = (MainViewTableViewCell *) [fromViewController.tableView cellForRowAtIndexPath:fromViewController.tableView.indexPathForSelectedRow];
     
-    CGRect cellFrame = cell.iv.frame;
-    
-    UIImageView *fakeCellToAnimate = cell.iv;
-
-    fakeCellToAnimate.frame = [fromViewController.view convertRect:fakeCellToAnimate.frame fromView:cell];
-    fakeCellToAnimate.clipsToBounds = YES;
-    fakeCellToAnimate.layer.cornerRadius = CGRectGetHeight(fakeCellToAnimate.frame)/2;
-    
     UIView *contextView = [transitionContext containerView];
-    
     [contextView addSubview:fromViewController.view];
     [contextView addSubview:toViewController.view];
-    [contextView addSubview:fakeCellToAnimate];
-
-    CGFloat sizeToMove = toViewController.view.frame.size.width * 2;
+    
+    CGFloat sizeToMove = toViewController.view.frame.size.width * 3;
     
     toViewController.view.center = CGPointMake(toViewController.view.center.x + sizeToMove,
                                                toViewController.view.center.y);
-
-    
     [UIView animateWithDuration:totalDuration
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseIn
                      animations:^{
                          toViewController.showImage.image = cell.iv.image;
-                         toViewController.frameOfCellFromMainTable = cell.iv.frame;
-                         toViewController.indexPathFromCell = fromViewController.tableView.indexPathForSelectedRow;
-                         
-                         fakeCellToAnimate.frame = CGRectMake(fromViewController.view.frame.origin.x,
-                                                    fakeCellToAnimate.frame.origin.y,
-                                                    toViewController.view.frame.size.width,
-                                                    toViewController.showImage.frame.size.height);
+                         fromViewController.view.transform = CGAffineTransformMakeScale(0.5, 0.5);
+                         //fromViewController.navigationController.view.transform  = CGAffineTransformMakeScale(0.5, 0.5);
                      } completion:^(BOOL finished) {
                          
                          [UIView animateWithDuration:totalDuration/2
                                                delay:0.0
-                                             options:UIViewAnimationOptionCurveEaseInOut
+                              usingSpringWithDamping:0.8
+                               initialSpringVelocity:0.1
+                                             options:0
                                           animations:^{
-        
                                               toViewController.view.center = CGPointMake(toViewController.view.center.x - sizeToMove,
                                                                                          toViewController.view.center.y);
-                                              fakeCellToAnimate.layer.cornerRadius = 0.0f;
-                                              
                                           }
                                           completion:^(BOOL finished) {
                                               [transitionContext completeTransition:YES];
-                                              cell.iv.frame = cellFrame;
-                                              cell.iv.layer.cornerRadius = 20.0f;
+                                              fromViewController.view.transform = CGAffineTransformIdentity;
                                           }];
                      }];
 }
@@ -104,43 +85,26 @@ static CGFloat const totalDuration = 0.3;
     [contextView addSubview:toViewController.view];
     [contextView addSubview:fromViewController.view];
     
-    //toViewController.view.transform = CGAffineTransformMakeScale(0.5, 0.5);
-    //UIImageView *fakeCellToAnimate = fromViewController.showImage;
+    toViewController.view.transform = CGAffineTransformMakeScale(0.5, 0.5);
     
     CGFloat sizeToMove = fromViewController.view.frame.size.width * 3;
     
-    //CGRect newFrame = [toViewController.view convertRect:fakeCellToAnimate.frame fromView:fromViewController];
-
-    //fromViewController.view.alpha = 0.0f;
-    
-    [UIView animateWithDuration:totalDuration + 3
+    [UIView animateWithDuration:totalDuration/2
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseIn
                      animations:^{
                          fromViewController.view.center = CGPointMake(fromViewController.view.center.x - sizeToMove,
                                                                       fromViewController.view.center.y);
-                         
-//                         fakeCellToAnimate.frame = CGRectMake(newFrame.origin.x - fromViewController.frameOfCellFromMainTable.origin.x,
-//                                                              newFrame.origin.y - fromViewController.frameOfCellFromMainTable.origin.y,
-//                                                              fromViewController.frameOfCellFromMainTable.size.width,
-//                                                              fromViewController.frameOfCellFromMainTable.size.height);
-                         
                      } completion:^(BOOL finished) {
                          [UIView animateWithDuration:totalDuration/2
                                                delay:0.0
                                              options:UIViewAnimationOptionCurveEaseOut
                                           animations:^{
-                                              
-                                              //toViewController.view.transform = CGAffineTransformIdentity;
-
-                                              
+                                              toViewController.view.transform = CGAffineTransformIdentity;
                                           } completion:^(BOOL finished) {
-                                              
                                               [transitionContext completeTransition:YES];
                                               
                                           }];
                      }];
 }
-
-
 @end
